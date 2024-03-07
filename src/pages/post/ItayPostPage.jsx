603c3c618c3ce2f2a0386043dff1b431ba2990b3
Adir Dahari <Adir10500@gmail.com>
2024-03-07 20:59:56 +0200

    update

--- src/pages/post/ItayPostPage.jsx
+++ src/pages/post/ItayPostPage.jsx
@@ -1,88 +1,147 @@
-import { Box, Grid, Button } from "@mui/material";
+import { Box, Grid, Divider, Button } from "@mui/material";
 import "../../style/postPage.css";
+import { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import MyToast from "../../messages/MyToast";
 
 const ItayPostPage = () => {
+  const { id: _id } = useParams();
+  const [dataFromServer, setDataFromServer] = useState(null);
+  const [imageIndex, setImageIndex] = useState(0);
+  const [mainImage, setMainImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(`/posts/${_id}`);
+        console.log(data);
+        setDataFromServer(data);
+        setMainImage(data.game.images[0].url);
+      } catch (err) {
+        MyToast.error("Something wrong, Please try again later");
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  const handleImageIndexChange = (e) => {
+    console.log(e.target.id);
+    setImageIndex(e.target.id);
+    setMainImage(dataFromServer.game.images[e.target.id].url);
+  };
+
   return (
     <Box pt={8}>
-      <Box sx={{ maxWidth: 1200 }} className="postpage-container-copy">
-        <Grid container>
-          <Grid
-            item
-            xs={12}
-            sm={12}
-            md={6}
-            sx={{ display: "flex", minHeight: 600, alignItems: "center" }}
-          >
-            <Box className="postpage-image-list">
-              <Box
-                className="post-image-item"
-                sx={{
-                  background: `linear-gradient(0deg, rgba(255, 255, 255, 0.28) 0%, rgba(255, 255, 255, 0.28) 100%), url(../../../public/assets/img/post/img_frame_9.png), white 50% / cover no-repeat`,
-                  backgroundRepeat: "no-repeat",
-                  backgroundPosition: "center",
-                  backgroundSize: "contain",
-                }}
-              ></Box>
-              <Box
-                className="post-image-item"
-                sx={{
-                  background: `linear-gradient(0deg, rgba(255, 255, 255, 0.28) 0%, rgba(255, 255, 255, 0.28) 100%), url(../../../public/assets/img/post/img_frame_9.png), white 50% / cover no-repeat`,
-                  backgroundRepeat: "no-repeat",
-                  backgroundPosition: "center",
-                  backgroundSize: "contain",
-                }}
-              ></Box>
-              <Box
-                className="post-image-item"
-                sx={{
-                  background: `url(../../../public/assets/img/post/img_frame_9.png), white 50%`,
-                  backgroundRepeat: "no-repeat",
-                  backgroundPosition: "center",
-                  backgroundSize: "contain",
-                }}
-              ></Box>
-            </Box>
-            <Box
+      <Box sx={{ maxWidth: 1200 }} className="postpage-container">
+        {dataFromServer && (
+          <Grid container sx={{ pb: 4, pt: 4 }}>
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              md={6}
               sx={{
-                background: `url(../../../public/assets/img/post/img_frame_9.png), white 50%`,
-                backgroundRepeat: "no-repeat",
-                m: "0 auto",
-                backgroundPosition: "center",
-                backgroundSize: "contain",
+                display: "flex",
+                minHeight: 600,
+                alignItems: "center",
+                flexDirection: { xs: "column-reverse", sm: "row" },
               }}
-              className="postpage-main-image"
-            ></Box>
-          </Grid>
-          <Grid item xs={12} sm={12} md={6} sx={{ height: 600 }}>
-            <Box className="postpage-details-top">
-              <Box className="postpage-details-title">
-                <p className="postpage-details-title-text">Title</p>
+            >
+              <Box className="postpage-image-list">
+                {dataFromServer.game.images.map((img, index) => (
+                  <Fragment key={img.url}>
+                    <Box
+                      id={index}
+                      onClick={handleImageIndexChange}
+                      className="post-image-item"
+                      sx={{
+                        background:
+                          index == imageIndex
+                            ? `url(${img.url}), white 50%`
+                            : `linear-gradient(0deg, rgba(255, 255, 255, 0.28) 0%, rgba(255, 255, 255, 0.28) 100%), url(${img.url}), white 50% / cover no-repeat`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "center",
+                        backgroundSize: "contain",
+                      }}
+                    ></Box>
+                    {index !== dataFromServer.game.images.length - 1 && (
+                      <Divider />
+                    )}
+                  </Fragment>
+                ))}
               </Box>
 
-              <Box className="postpage-details-price-container">
-                <p className="postpage-details-price-text">₪170</p>
+              <Box
+                sx={{
+                  background: `url(${mainImage}), white 50%`,
+                  backgroundRepeat: "no-repeat",
+                  m: "0 auto",
+                  backgroundPosition: "center",
+                  backgroundSize: "contain",
+                }}
+                className="postpage-main-image"
+              ></Box>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} sx={{ minHeight: 600 }}>
+              <Box className="postpage-details-top">
+                <Box className="postpage-details-title">
+                  <p className="postpage-details-title-text">
+                    {dataFromServer.game.name}
+                  </p>
+                </Box>
+
+                <Box className="postpage-details-price-container">
+                  <p className="postpage-details-price-text">
+                    ₪{dataFromServer.game.price}
+                  </p>
+                </Box>
               </Box>
-            </Box>
-            <Box className="postpage-details-cate">
-              <p className="postpage-details-cate-title">Category:</p>
-              <p className="postpage-details-cate-des">Lorem ipsum dolor</p>
-            </Box>
-            <Box className="postpage-details-bottom">
-              <p className="postpage-details-bottom-title">Description</p>
-              <p className="postpage-details-bottom-des">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                animi ipsum amet deleniti ex doloribus corporis eligendi?
-                Incidunt dignissimos corrupti eius vitae dolorem quo quasi
-                dolorum, asperiores quos, facilis voluptatem!
-              </p>
-            </Box>
-            <Box sx={{ pr: 4, pt: 4 }}>
-              <button className="postpage-btn">
-                <p className="postpage-btn-text">Contact Seller</p>
-              </button>
-            </Box>
+              <Box className="postpage-details-cate">
+                <p className="postpage-details-cate-title">Category:</p>
+                {dataFromServer.game.category.map((cate, index) => (
+                  <p
+                    className="postpage-details-cate-des"
+                    key={cate + index}
+                    display="inline"
+                  >
+                    {cate +
+                      (index < dataFromServer.game.category.length - 1
+                        ? " | "
+                        : "")}
+                  </p>
+                ))}
+              </Box>
+              {dataFromServer.game.description ? (
+                <Box className="postpage-details-bottom">
+                  <p className="postpage-details-bottom-title">Description</p>
+                  <p className="postpage-details-bottom-des">
+                    {dataFromServer.game.description}
+                  </p>
+                </Box>
+              ) : (
+                <Box sx={{ minHeight: "220px" }}></Box>
+              )}
+
+              <Box sx={{ pr: { xs: 4, sm: 8 }, pt: 4 }}>
+                <Button
+                  variant="contained"
+                  sx={{
+                    float: "right",
+                    borderRadius: "50px",
+                    pr: 5,
+                    pl: 5,
+                    bgcolor: "#000",
+                    color: "white",
+                    boxShadow: `0px 4.739px 23.694px 0px rgba(0, 0, 0, 0.25)`,
+                  }}
+                >
+                  <p className="postpage-btn-text">Contact Seller</p>
+                </Button>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
+        )}
       </Box>
     </Box>
   );
